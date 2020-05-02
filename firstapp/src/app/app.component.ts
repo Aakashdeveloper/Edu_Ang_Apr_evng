880cf6b475932047f7d9ab5f9b8cc4ffab36681d
aakashDeveloper <ahanda205@gmail.com>
2020-05-02 18:56:09 +0100

    a

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent{
-
+    sampleColor = 'red';
 }
--- firstapp/src/app/app.module.ts
+++ firstapp/src/app/app.module.ts
@@ -3,19 +3,24 @@ import {NgModule} from '@angular/core';
 // To display content on the browser
 import {BrowserModule} from '@angular/platform-browser';
 
-
 // Main component
 import { AppComponent } from './app.component';
+// For Forms Element
+import { FormsModule } from '@angular/forms';
 
 import { FooterComponent } from './Footer/Footer.component';
 import { HeaderComponent } from './Header/Header.component';
 import { ProductComponent } from './product/product.component';
+import { MyUpperPipe } from './product/myupper.pipe';
+import { MyDiscount } from './product/mydiscount.pipe';
+
 
 // Decorater(matadata)
 @NgModule({
     // All module will declare her
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ],
 
     // All component and pipe
@@ -23,7 +28,9 @@ import { ProductComponent } from './product/product.component';
         AppComponent,
         FooterComponent,
         HeaderComponent,
-        ProductComponent
+        ProductComponent,
+        MyUpperPipe,
+        MyDiscount
     ],
 
     // Only and only first/main coomponent
